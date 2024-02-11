263bf470dad51915577676ca39ef9add3c9d8ee6
chandlereskey <chaneskey@gmail.com>
2024-02-11 15:39:47 -0500

    i think this is everything that i need?? will test it tonight

--- actions.py
+++ actions.py
@@ -1,13 +1,27 @@
-"""
-This method takes a stock ticker and the
-current amount of money available in robinhood
+from robin_stocks import robinhood as r
+from lstm_model import retrain_and_predict
 
-@param ticker: ticker string
-@param available_money: integer value of money
-@return number of stocks bought
+# set up global variables
+ticker = None
+predicted_increase = None
+money_spent = 0
+end_amount = 0
+
 """
-def BUY(ticker: str, available_money: int):
-    pass
+    This method takes a stock ticker and the
+    current amount of money available in robinhood
+    
+    @param ticker: ticker string
+    @param available_money: integer value of money
+    @return number of stocks bought
+"""
+def BUY():
+    if ticker:
+        equity = r.profiles.load_portfolio_profile()['equity']
+        # buy stock
+        global money_spent
+        buying = r.orders.order_buy_fractional_by_price(symbol=ticker, amountInDollars=equity)
+        money_spent = equity
 
 
 """
@@ -19,37 +33,48 @@ sold and the amount of money in the account.
 @return returns the time sold and the amount sold for 
         (or money in the account)
 """
-def SELL(ticker: str):
-    pass
-
+def SELL():
+    if ticker:
+        global end_amount
+        end_amount = r.profiles.load_portfolio_profile()['market_value']
+        r.orders.order_sell_fractional_by_price(symbol=ticker, amountInDollars=end_amount)
+    global ticker
+    ticker = None
 
 """
 This method retrains the model based on the 
-current day's results.
+current day's results and reruns the model to get
+following day prediction.
+
+This will run the model on each stock ticker and get the
+predicted percent increase
+
+It will set the value of the ticker
 """
-def retrain_model():
-    pass
-
-
-"""
-This method runs the model and gets the results 
-being the stock ticker chosen along with the prediction.
-
-@return ticker and predicted percent increase
-"""
-def choose_stock():
-    pass
-
+def retrain_model_and_get_next_day_stock():
+    stocks = {'AAPL': 0, 'MSFT': 0, 'NVDA': 0, 'NBIX': 0, 'DXCM': 0}
+    for stock in stocks.keys():
+        stocks[stock] = retrain_and_predict(stock)
+    global ticker
+    global predicted_increase
+    ticker = max(stocks, key=stocks.get)
+    predicted_increase = stocks[ticker]
+    if predicted_increase < 1:
+        ticker = None
+        predicted_increase = None
 
 """
 This method makes an API call for the ticker
-passed in. 
+passed in. If the price has reached a threshold then will 
+call the SELL function.
 
 @param ticker: this is stock ticker value for the day
 @return current price
 """
-def check_price(ticker: str):
-    pass
+def check_price():
+    current_money = r.profiles.load_portfolio_profile()['market_value']
+    if current_money/money_spent >= predicted_increase:
+        SELL()
 
 
 """
@@ -57,4 +82,4 @@ This method saves the daily results into a file
 at the end of the day.
 """
 def daily_results():
-    pass
+    print('Start cost: ', money_spent, 'ending amount: ', end_amount)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # this file is the main file to run the entire application
 import datetime
+import os
 
 # while True:
 #     current_time = datetime.datetime.now() # need to make this simplified to just hour
@@ -9,17 +10,28 @@ import datetime
 import schedule
 import time
 
-value = ''
-def job1(test):
-    print('testing:', test)
+from robin_stocks import robinhood as r
+
+from actions import *
+
+username = os.getenv('USER_NAME')
+password = os.getenv('PASSWORD')
+login = r.authentication.login(username, password)
 
 # Schedule your functions to be called at specific hours
-schedule.every().hour.at(":33").do(lambda: job1(value))
-#schedule.every().hour.at(":00").do(job2)
+schedule.every().hour.at("09:00").do(lambda: BUY())
+schedule.every().hour.at("10:00").do(lambda: check_price())
+schedule.every().hour.at("11:00").do(lambda: check_price())
+schedule.every().hour.at("12:00").do(lambda: check_price())
+schedule.every().hour.at("13:00").do(lambda: check_price())
+schedule.every().hour.at("14:00").do(lambda: check_price())
+schedule.every().hour.at("15:00").do(lambda: check_price())
+schedule.every().hour.at("16:00").do(lambda: check_price())
+schedule.every().hour.at("16:55").do(lambda: SELL())
+schedule.every().hour.at("00:00").do(lambda: retrain_model_and_get_next_day_stock())
 
 # Keep the script running
 while True:
-    value = 'hello'
     schedule.run_pending()
     time.sleep(1)
 
