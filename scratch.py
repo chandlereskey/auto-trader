ae3fa84905b768a6647ea76657911d6ed3b33134
chandlereskey <chaneskey@gmail.com>
2024-02-14 15:37:45 -0500

    removing scratch code

--- scratch.py
+++ scratch.py
@@ -1,13 +1,1 @@
-import os
-
-from robin_stocks import robinhood as r
-username = os.getenv('USER_NAME')
-password = os.getenv('PASSWORD')
-login = r.authentication.login(username, password)
-
-ticker = 'AAPL'
-if ticker:
-    end_amount = float(r.profiles.load_portfolio_profile()['market_value'])
-    print('SELLING', end_amount, 'of', ticker)
-    selling = r.orders.order_sell_fractional_by_price(symbol=ticker, amountInDollars=end_amount)
-ticker = None
+# JUST AN EXTRA FILE FOR TESTING THINGS OUT
